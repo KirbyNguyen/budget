3a9105e9d96d86effb3138944497f55c24d35ed6
nicklui <nicklui1998@gmail.com>
2021-04-30 11:00:44 -0700

    added retrieve list of categories and single category to category services

--- lib/pages/category/CategoryPage.dart
+++ lib/pages/category/CategoryPage.dart
@@ -1,4 +1,10 @@
+import 'package:budget/models/Account.dart';
+import 'package:budget/models/Category.dart';
+import 'package:budget/pages/transaction/list_item.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:budget/services/AccountDatabaseServices.dart';
 import 'package:flutter/material.dart';
+import 'package:budget/pages/category/AddNewCategoryPage.dart';
 // import '../transaction/list_item.dart';
 
 class CategoryPage extends StatefulWidget {
@@ -7,6 +13,9 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
+  List<Category> transactionList = [];
+  // Authentication service
+  final FirebaseAuth auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/services/CategoryServices.dart
+++ lib/services/CategoryServices.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:budget/pages/category/AddNewCategoryPage.dart';
+import 'package:budget/models/Category.dart';
 
 class CategoryServices {
   final FirebaseAuth auth = FirebaseAuth.instance;
@@ -18,6 +20,48 @@ class CategoryServices {
       });
     } catch (error) {
       print(error.toString());
+      return null;
+    }
+  }
+
+// Retrieve categories that belong to the user
+  Future<dynamic> getCategories(uid) async {
+    List<Category> categoryList = [];
+    try {
+      QuerySnapshot eventsQuery =
+          await transactionCollection.where("uid", isEqualTo: uid).get();
+      eventsQuery.docs.forEach(
+        (document) {
+          Category temp = new Category(
+              name: document['categoryName'],
+              budget: document['categoryBudget'],
+              uid: document['userid'],
+              colors: document['Colors']);
+          categoryList.add(temp);
+        },
+      );
+      return categoryList;
+    } catch (error) {
+      print(error);
+      return null;
+    }
+  }
+
+  // Retrieve transaction
+  Future<dynamic> getTransaction(String id) async {
+    try {
+      DocumentSnapshot document = await transactionCollection.doc(id).get();
+
+      Category transaction = new Category(
+        name: document['categoryName'],
+        budget: document['categoryBudget'],
+        uid: document['userid'],
+        colors: document['Colors'],
+      );
+
+      return transaction;
+    } catch (error) {
+      print(error);
       return null;
     }
   }
